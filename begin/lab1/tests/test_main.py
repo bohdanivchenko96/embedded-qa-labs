586bb3a68b3d175331fac2ea5b73908f47a8aa6a
bohdanivchenko96 <bohdan@BohdanPc.localdomain>
2026-09-06 19:54:08 +0000

    Minor fix

--- begin/lab1/tests/test_main.py
+++ begin/lab1/tests/test_main.py
@@ -1,9 +1,9 @@
 def test_ping_message(serial_connection):
     serial_connection.write(b'PING\n')
     response = serial_connection.readline()
-    assert b'PONG' in response
+    assert response.strip() == b'PONG'
 
 def test_invalid_message(serial_connection):
     serial_connection.write(b'invalid\n')
     response = serial_connection.readline()
-    assert b'ERR unknown_command' in response
+    assert response.strip() == b'ERR unknown_command'
