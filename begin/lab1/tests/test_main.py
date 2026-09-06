854c535155b4ea0ede021f01cf6cc8cdb6a1e6a8
bohdanivchenko96 <bohdan@BohdanPc.localdomain>
2026-09-06 19:51:43 +0000

    Added unhappy scenario

--- begin/lab1/tests/test_main.py
+++ begin/lab1/tests/test_main.py
@@ -1,4 +1,9 @@
-def test_device_connection(serial_connection):
+def test_ping_message(serial_connection):
     serial_connection.write(b'PING\n')
     response = serial_connection.readline()
     assert b'PONG' in response
+
+def test_invalid_message(serial_connection):
+    serial_connection.write(b'invalid\n')
+    response = serial_connection.readline()
+    assert b'ERR unknown_command' in response
